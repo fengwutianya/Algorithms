b155cae539a8b415fe8e2886f43047b0dc050c63
fengwutianya <949979029@qq.com>
2017-03-21 12:31:42 +0800

    Red Black Tree

--- src/algorithms/search/RedBlackBST.java
+++ src/algorithms/search/RedBlackBST.java
@@ -24,6 +24,38 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    public void put(Key key, Value value) {
+        if (key == null) throw new NullPointerException("key cannot be null");
+        if (value == null) {
+            delete(key);
+            return;
+        }
+        root = put(root, key, value);
+        root.color = BLACK;
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        //插入一定是红节点，也就是说2-3树里不往叶子结点插，叶子结点只是装饰
+        if (node == null) return new Node(key, value, RED, 1);
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) node = put(node.left, key, value);
+        else if (cmp > 0) node = put(node.right, key, value);
+        else node.value = value;
+
+        //插入黑节点的右边或者红节点的右边
+        if (node.right.color == RED && node.left.color != RED) node = rotateLeft(node);
+        //4节点 全左
+        if (node.left.color == RED && node.left.left.color == RED) node = rotateRight(node);
+        //4节点，左右
+        if (node.left.color == RED && node.right.color == RED) flipColors(node);
+        //插入黑节点的左边，do nothing
+    }
+
+    private void delete(Key key) {
+        return 1;
+    }
+
     private Node rotateLeft(Node h) {
         Node x = h.right;
         h.right = x.left;
